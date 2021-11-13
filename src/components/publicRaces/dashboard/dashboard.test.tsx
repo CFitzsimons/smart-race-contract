6e8436c311f064cb0ae320b1fc4cae807e899816
Colin Fitzsimons <colin@cleancodecompany.com>
2021-11-13 03:16:19 +0000

    Added linting

--- src/components/publicRaces/dashboard/dashboard.test.tsx
+++ src/components/publicRaces/dashboard/dashboard.test.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { v4 as uuid } from 'uuid';
 
 import Dashboard from './index';
